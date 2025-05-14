b8e3815a6a390541feceadd63a758a1c1eb5c726
Amey Kode <ameykode2001@gmail.com>
2025-05-15 00:22:54 +0530

    Added health check

--- cmd/main.go
+++ cmd/main.go
@@ -6,11 +6,10 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"status": "ok",
 		})
 	})
-
 	r.Run()
 }
